52fe6932efe62f392f61e3ec5dff2ef591e5c63a
adilramzan <adil.ramzan192@gmail.com>
2024-12-14 04:21:08 +0500

    container config done

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import Stats from "@/components/Stats";
@@ -5,6 +7,18 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
+  // Handler to download resume
+  const handleDownload = () => {
+    // Assuming the PDF file is stored in the public folder as "Aadil_Resume.pdf"
+    const filePath = "/Aadil_Resume.pdf";
+
+    // Create an anchor element and trigger download
+    const link = document.createElement("a");
+    link.href = filePath;
+    link.download = "Aadil_Resume.pdf"; // Set the file name
+    link.click();
+  };
+
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
@@ -22,7 +36,11 @@ const Home = () => {
             </p>
             {/* button & socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" className="uppercase flex items-center gap-2 text-accent-default hover:text-white">
+              <Button
+                onClick={handleDownload}
+                variant="outline"
+                className="uppercase flex items-center gap-2 text-accent-default hover:text-white"
+              >
                 <span>Download Resume</span>
                 <FiDownload className="text-xl" />
               </Button>
